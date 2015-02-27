b47329f60b9d395f71fc9137d8ed098d4b0df8f0
Alexandru Ciobanu <ca.work@outlook.com>
2015-02-27 16:53:13 +0000

    Experimentation...

---
+++ XtraLiteTemplates/Utils/BranchedDictionary.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtraLiteTemplates.Utils
+{
+    public class BranchedDictionary<TKey, TValue> : 
+        IDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>, IDictionary
+    {
+        private sealed class KeyCollection : ICollection<TKey>, ICollection
+        {
+            private BranchedDictionary<TKey, TValue> m_owner;
+
+            public KeyCollection(BranchedDictionary<TKey, TValue> dictionary)
+            {
+                Debug.Assert(dictionary != null);
+                m_owner = dictionary;
+            }
+
+            public Int32 Count
+            {
+                get
+                {
+                    return m_owner.Count;
+                }
+            }
+
+            public void CopyTo(TKey[] array, int index)
+            {
+
+            }
+
+            public Dictionary<TKey, TValue>.KeyCollection.Enumerator GetEnumerator();
+            public struct Enumerator : IEnumerator<TKey>, IEnumerator, IDisposable
+            {
+                public TKey Current { get; }
+                public void Dispose();
+                public bool MoveNext();
+            }
+
+            public void Add(TKey item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Clear()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool Contains(TKey item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsReadOnly
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool Remove(TKey item)
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator<TKey> IEnumerable<TKey>.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private BranchedDictionary<TKey, TValue> m_parentDictionary;
+        private Dictionary<TKey, TValue> m_dictionary;
+        private HashSet<TKey> m_removed;
+
+        private BranchedDictionary(BranchedDictionary<TKey, TValue> parent)
+        {
+            Debug.Assert(parent != null);
+
+            m_parentDictionary = parent;
+            m_dictionary = new Dictionary<TKey, TValue>(parent.Comparer);
+            m_removed = new HashSet<TKey>();
+        }
+
+        public BranchedDictionary()
+            : this(EqualityComparer<TKey>.Default)
+        {
+        }
+
+        public BranchedDictionary(IEqualityComparer<TKey> comparer)
+        {
+            ValidationHelper.AssertArgumentIsNotNull("comparer", comparer);
+
+            m_dictionary = new Dictionary<TKey, TValue>(comparer);
+            m_removed = new HashSet<TKey>();
+        }
+
+        public BranchedDictionary(IReadOnlyDictionary<TKey, TValue> dictionary) : this()
+        {
+            ValidationHelper.AssertArgumentIsNotNull("dictionary", dictionary);
+            foreach (var kvp in dictionary)
+                Add(kvp);
+        }
+
+        public BranchedDictionary(IReadOnlyDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer)
+            : this(comparer)
+        {
+            ValidationHelper.AssertArgumentIsNotNull("dictionary", dictionary);
+            foreach (var kvp in dictionary)
+                Add(kvp);
+        }
+
+        public IEqualityComparer<TKey> Comparer
+        { 
+            get
+            {
+                return m_dictionary.Comparer;
+            }
+        }
+
+        public BranchedDictionary<TKey, TValue> AddBranch(IReadOnlyDictionary<TKey, TValue> dictionary)
+        {
+            ValidationHelper.AssertArgumentIsNotNull("dictionary", dictionary);
+
+            var branch = AddBranch();
+            foreach (var kvp in dictionary)
+                branch.Add(kvp);
+
+            return branch;
+        }
+
+        public BranchedDictionary<TKey, TValue> AddBranch()
+        {
+            return new BranchedDictionary<TKey, TValue>(this);
+        }
+
+
+
+        public void Add(TKey key, TValue value)
+        {
+            ValidationHelper.AssertArgumentIsNotNull("key", key);
+            ValidationHelper.Assert("key", "Key not present in the dictionary or its parent branch.", !ContainsKey(key));
+
+            m_dictionary.Add(key, value);
+            m_removed.Remove(key);
+        }
+
+        public Boolean ContainsKey(TKey key)
+        {
+            ValidationHelper.AssertArgumentIsNotNull("key", key);
+
+            if (m_removed.Contains(key))
+                return false;
+            else
+                return m_dictionary.ContainsKey(key) || m_parentDictionary.ContainsKey(key);
+        }
+
+        public Boolean Remove(TKey key)
+        {
+            ValidationHelper.AssertArgumentIsNotNull("key", key);
+
+            var removed = m_dictionary.Remove(key);
+            if (!removed && !m_removed.Contains(key))
+            {
+                /* No such value in *this* branch. Check the parent branch and mark as deleted if found. */
+                removed = m_parentDictionary.ContainsKey(key);
+                if (removed)
+                    m_removed.Add(key);
+            }
+
+            return removed;
+        }
+
+        public Boolean TryGetValue(TKey key, out TValue value)
+        {
+            ValidationHelper.AssertArgumentIsNotNull("key", key);
+
+            if (m_dictionary.TryGetValue(key, out value))
+                return true;
+            else if (!m_removed.Contains(key) && m_parentDictionary.TryGetValue(key, out value))
+                return true;
+            else
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get 
+            {
+                return this.Select(kvp => kvp.Key)
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Add(object key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        IDictionaryEnumerator IDictionary.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFixedSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        ICollection IDictionary.Keys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Remove(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        ICollection IDictionary.Values
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object this[object key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ XtraLiteTemplates/Utils/TypeMemberAccessor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace XtraLiteTemplates.Utils
+{
+    public sealed class TypeMemberAccessor
+    {
+        private IDictionary<String, Func<Object, Object>> m_readers;
+        private Boolean m_caseSensitive;
+
+        private String AdjustNameCasing(String name)
+        {
+            Debug.Assert(!String.IsNullOrEmpty(name));
+
+            if (m_caseSensitive)
+                return name;
+            else
+                return name.ToUpperInvariant();
+        }
+
+        public TypeMemberAccessor(Type type, Boolean caseSensitive)
+        {
+            Debug.Assert(type != null);
+
+            m_caseSensitive = caseSensitive;
+
+            /* Simply scan the type for properties and parameterless methods. */
+            var m_readers = new Dictionary<String, Func<Object, Object>>();
+
+            foreach (var field in type.GetRuntimeFields())
+            {
+                if (field.IsPublic)
+                    m_readers[AdjustNameCasing(field.Name)] = target => field.GetValue(target);
+            }
+            foreach (var property in type.GetRuntimeProperties())
+            {
+                if (property.CanRead && property.GetIndexParameters().Length == 0)
+                    m_readers[AdjustNameCasing(property.Name)] = target => property.GetValue(target);
+            }
+            foreach (var method in type.GetRuntimeMethods())
+            {
+                if (method.IsPublic && method.ReturnParameter != null && method.GetParameters().Length == 0)
+                    m_readers[AdjustNameCasing(method.Name)] = target => method.Invoke(target, null);
+            }
+        }
+
+        public Object Read(Object target, String propertyName, Object defaultValue = null)
+        {
+            Debug.Assert(target != null);
+            Debug.Assert(!String.IsNullOrEmpty(propertyName));
+
+            Func<Object, Object> reader;
+            if (m_readers.TryGetValue(AdjustNameCasing(propertyName), out reader))
+                return reader(target);
+            else
+                return defaultValue;
+        }
+
+        public String Read(Object target, String propertyName, String defaultValue = null)
+        {
+            Object value = Read(target, propertyName, defaultValue);
+            if (value != null)
+                return value.ToString();
+            else
+                return null;
+        }
+    }
+}
