fb6c6fdee79207ef10381ffc9f8bf7b9fd629a45
Alexandru Ciobanu <ca.work@outlook.com>
2015-05-23 14:51:51 +0100

    One more down.

--- XtraLiteTemplates/SimpleTypeDisemboweler.cs
+++ XtraLiteTemplates/SimpleTypeDisemboweler.cs
@@ -7,33 +7,13 @@ using System.Threading.Tasks;
 
 namespace XtraLiteTemplates
 {
+    /// <summary>
+    /// Utility class that allows for easy access to a type's properties and methods.
+    /// This class is used internally to implement the member access operator.
+    /// </summary>
     public sealed class SimpleTypeDisemboweler
     {
-        [Flags]
-        public enum EvaluationOptions
-        {
-            None = 0,
-            TreatParameterlessFunctionsAsProperties = 1,
-            TreatAllErrorsAsNull = 2,
-        }
-        
         private IReadOnlyDictionary<String, Func<Object, Object>> m_mapping;
-
-        public Type Type { get; private set; }
-        public IEqualityComparer<String> Comparer { get; private set; }
-        public EvaluationOptions Options { get; private set; }
-
-        public SimpleTypeDisemboweler(Type type, EvaluationOptions options, IEqualityComparer<String> memberComparer)
-        {
-            Expect.NotNull("type", type);
-            Expect.NotNull("memberComparer", memberComparer);
-
-            Type = type;
-            Comparer = memberComparer;
-            Options = options;
-
-            m_mapping = BuildMapping();
-        }
 
         private IReadOnlyDictionary<String, Func<Object, Object>> BuildMapping()
         {
@@ -55,7 +35,7 @@ namespace XtraLiteTemplates
                 /* Load methods in. */
                 foreach (var method in Type.GetMethods())
                 {
-                    if (method.GetParameters().Length > 0 || method.IsAbstract || method.IsConstructor || 
+                    if (method.GetParameters().Length > 0 || method.IsAbstract || method.IsConstructor ||
                         method.IsPrivate)
                         continue;
 
@@ -67,6 +47,77 @@ namespace XtraLiteTemplates
             return mapping;
         }
 
+        /// <summary>
+        /// Defines a set of options that guides the <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/>.
+        /// </summary>
+        [Flags]
+        public enum EvaluationOptions
+        {
+            /// <summary>
+            /// No special options.
+            /// </summary>
+            None = 0,
+            /// <summary>
+            /// Instructs the <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/> to treat parameterless, methods to be treated as properties.
+            /// The <c>void</c> return type will be treated as <c>null</c>.
+            /// </summary>
+            TreatParameterlessFunctionsAsProperties = 1,
+            /// <summary>
+            /// Instructs the <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/> to silently return <c>null</c> for any property that raised an exception
+            /// during evaluation.
+            /// </summary>
+            TreatAllErrorsAsNull = 2,
+        }
+
+        /// <summary>
+        /// <value>The <see cref="System.Type"/> that represents the type being inspected.</value>
+        /// <remarks>Value provided by the caller during construction.</remarks>
+        /// </summary>
+        public Type Type { get; private set; }
+
+        /// <summary>
+        /// <value>An instance of <see cref="System.Collections.Generic.IEqualityComparer{String}"/> used by <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/> to compare the 
+        /// names of properies. This property is primarily used to decide the case-sensitivity of this <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/> instance.</value>
+        /// <remarks>Value provided by the caller during construction.</remarks>
+        /// </summary>
+        public IEqualityComparer<String> Comparer { get; private set; }
+
+        /// <summary>
+        /// <value>A set of <see cref="XtraLiteTemplates.SimpleTypeDisemboweler.EvaluationOptions"/> options that modifies the bahaviour of this 
+        /// <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/> instance.</value>
+        /// <remarks>Value provided by the caller during construction.</remarks>
+        /// </summary>
+        public EvaluationOptions Options { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/> class.
+        /// </summary>
+        /// <param name="type">The <see cref="System.Type"/> to inspect.</param>
+        /// <param name="options">A set of <see cref="XtraLiteTemplates.SimpleTypeDisemboweler.EvaluationOptions"/> options.</param>
+        /// <param name="memberComparer">An instance of <see cref="System.Collections.Generic.IEqualityComparer{String}"/> used when looking up properties in the inspected type.</param>
+        /// <exception cref="System.ArgumentNullException">Either <paramref name="type"/> or <paramref name="memberComparer"/> parameters are <c>null</c>.</exception>
+        public SimpleTypeDisemboweler(Type type, EvaluationOptions options, IEqualityComparer<String> memberComparer)
+        {
+            Expect.NotNull("type", type);
+            Expect.NotNull("memberComparer", memberComparer);
+
+            Type = type;
+            Comparer = memberComparer;
+            Options = options;
+
+            m_mapping = BuildMapping();
+        }
+
+        /// <summary>
+        /// Reads the <paramref name="property"/> of <paramref name="instance"/>.
+        /// </summary>
+        /// <param name="property">The property name.</param>
+        /// <param name="instance">The object whose property is being read.</param>
+        /// <exception cref="System.ArgumentNullException"><paramref name="property"/> is <c>null</c>. The <paramref name="instance"/> is <c>null</c> and this instance of <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/>
+        /// is not instructed to ignore evaluation errors.</exception>
+        /// <exception cref="System.ArgumentException"><paramref name="property"/> is not a valid identifier.</exception>
+        /// <exception cref="XtraLiteTemplates.Evaluation.EvaluationException">Read error while reading the property value; and this instance of <see cref="XtraLiteTemplates.SimpleTypeDisemboweler"/>
+        /// is not instructed to ignore evaluation errors.</exception>
         public Object Read(String property, Object instance)
         {
             Expect.Identifier("property", property);
